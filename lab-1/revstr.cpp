908e47ae74ab0f5c405c4592d2a6a390d4b4eefb
Vishwa-J <107062706+Vishwa-J@users.noreply.github.com>
2022-08-19 20:30:43 +0530

    Add files via upload

---
+++ lab-1/revstr.cpp
@@ -0,0 +1,15 @@
+// Question-8
+#include<bits/stdc++.h>
+using namespace std;
+void reverse(string str){
+    if(str.size()==0){
+        return ;
+    }
+    reverse(str.substr(1));
+    cout<<str[0];
+}
+int main(){
+    string str="reverse";
+    reverse(str);
+    return 0;
+}
