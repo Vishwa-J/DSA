45f4d525cac5c022643de468d232bffdfa0cb9d9
Vishwa-J <107062706+Vishwa-J@users.noreply.github.com>
2022-10-07 22:16:04 +0530

    Add files via upload

---
+++ lab-5/Q8.cpp
@@ -0,0 +1,53 @@
+// To implement Radix sort
+#include <iostream>
+using namespace std;
+
+int getMax(int arr[], int n)
+{
+	int max = arr[0];
+	for (int i = 1; i < n; i++)
+		if (arr[i] > max)
+			max = arr[i];
+	return max;
+}
+void countSort(int arr[], int n, int exp)
+{
+	int output[n];
+	int i, count[10] = { 0 };
+
+	for (i = 0; i < n; i++)
+		count[(arr[i] / exp) % 10]++;
+
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (i = n - 1; i >= 0; i--) {
+		output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+		count[(arr[i] / exp) % 10]--;
+	}
+
+	for (i = 0; i < n; i++)
+		arr[i] = output[i];
+}
+
+void radixsort(int arr[], int n)
+{
+	int m = getMax(arr, n);
+	for (int exp = 1; m / exp > 0; exp *= 10)
+		countSort(arr, n, exp);
+}
+
+void printArr(int arr[], int n)
+{
+	for (int k = 0; k < n; k++)
+		cout << arr[k] << " ";
+}
+
+int main()
+{
+	int arr[] = { 547, 21, 365, 4, 87, 15, 36, 45};
+	int n = sizeof(arr) / sizeof(arr[0]);
+	radixsort(arr, n);
+	printArr(arr, n);
+	return 0;
+}
